431fa122c244d768c1fa9ea5aaba8c3f6ecd4ecb
jmalvarez97 <jualvarez@fi.uba.ar>
2023-06-21 17:49:43 -0300

    base

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import QrReader from 'react-qr-scanner';
 
 function App() {
+  const qrRef = useRef(null);
+
+  const handleScan = (data) => {
+    if (data) {
+      console.log(data.text)
+      alert(`ID escaneado: ${data.text}`);
+    }
+  };
+
+  const handleError = (error) => {
+    console.error(error);
+  };
+
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Choo Choo! This is an example of a create-react-app site running on Railway.</p>
-        <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
-      </header>
-    </div>
+      <h1>Escáner de QR</h1>
+      <QrReader
+        ref={qrRef}
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        constraints={{
+          video: { facingMode: "environment" }
+        }}
+        style={{ width: '70%' }}
+      />
+</div>
   );
 }
 
