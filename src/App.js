c1b94279ca77bf8cc232b154bad2296b4325c800
jmalvarez97 <jualvarez@fi.uba.ar>
2023-06-22 21:29:07 -0300

    resuelvo

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import "./App.css"
 import React, { useRef, useState } from 'react';
 import QrReader from 'react-qr-scanner';
-<<<<<<< HEAD
 import { getPaymentsOfID } from "./utils/querys";
-=======
-import { getPaymentsOfID } from './utils/querys';
-import "./App.css"
-
->>>>>>> eca476c (tabla, muestra pagos)
 
 function App() {
   const qrRef = useRef(null);
   const [id, setId] = useState("");
   const [pagos, setPagos] = useState([])
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-  const handleScan = (data) => {
-    if (data) {
-      setId(data.text);
-      getPaymentsOfID(data.text, setPagos);
-=======
-=======
-  
->>>>>>> 51e2276 (act)
-  const handleScan = (data) => {
+    const handleScan = (data) => {
     if (data) {
       setId(data.text)
       getPaymentsOfID(data.text, setPagos)
->>>>>>> eca476c (tabla, muestra pagos)
       alert(`ID escaneado: ${data.text}`);
     }
   };
@@ -50,7 +33,6 @@ function App() {
         constraints={{
           video: { facingMode: "environment" }
         }}
-<<<<<<< HEAD
         style={{ width: "400px" }}
       />
       <div className="id"> ID: {id}</div>
@@ -73,28 +55,6 @@ function App() {
     
     </table>
       
-=======
-        style={{ width: '400px' }}
-      />
-      <div className='id'>ID: {id}</div>
-      
-      <table>
-    <tr>
-      <th>Fecha</th>
-      <th>Monto</th>
-    </tr>
-    
-    {pagos.map((p) => {
-      return <tr>
-        <td>{p.date}</td>
-        <td>{p.amount}</td>
-      </tr>
-    })}
-    </table>
-
-
-
->>>>>>> eca476c (tabla, muestra pagos)
     </div>
   );
 }
--- src/utils/querys.js
+++ src/utils/querys.js
@@ -1,17 +1,9 @@
 const URL_API = "https://expressjs-mongoose-production-14de.up.railway.app/"
-<<<<<<< HEAD
-
-=======
->>>>>>> eca476c (tabla, muestra pagos)
 export async function getPaymentsOfID(id, state) {
     if (id) {
         try {
             const response = await fetch(URL_API + "socio/payments/" + id);
             const data = await response.json();
-<<<<<<< HEAD
-            console.log(data)
-=======
->>>>>>> eca476c (tabla, muestra pagos)
             state(data);
         } 
         catch (error) {
